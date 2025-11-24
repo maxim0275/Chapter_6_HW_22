17a8b16cbef8362014fdb42f3472cc369c7858ff
maxim0275 <maxim0275@yandex.ru>
2025-11-24 14:55:35 +0500

    final

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -6,15 +6,28 @@ class Command(BaseCommand):
     # help = 'Добавление тестовых продуктов'
 
     def handle(self, *args, **kwargs):
-        cat_1 = Category.objects.get(id=1)
         Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        categories = [
+            {'name': 'Основания', 'description': 'Основания для ракеток'}
+        ]
+
+        for category in categories:
+            add_category = Category(
+                name=category['name'],
+                description=category['description']
+            )
+            add_category.save()
+
+        add_category = Category.objects.get(name='Основания')
 
         products = [
-            {'name': 'Основание Peter Korbel', 'description': 'Основание Peter Korbel, Japan', 'category': cat_1,
+            {'name': 'Основание Peter Korbel', 'description': 'Основание Peter Korbel, Japan', 'category': add_category,
              'price_sale': 15000, 'created_at': '2025-11-22'},
-            {'name': 'Основание Timo Boll ALC', 'description': 'Основание TImo Boll, Japan', 'category': cat_1,
+            {'name': 'Основание Timo Boll ALC', 'description': 'Основание TImo Boll, Japan', 'category': add_category,
              'price_sale': 25000, 'created_at': '2025-11-22'},
-            {'name': 'Основание Timo Boll ZLC', 'description': 'Основание TImo Boll, Japan', 'category': cat_1,
+            {'name': 'Основание Timo Boll ZLC', 'description': 'Основание TImo Boll, Japan', 'category': add_category,
              'price_sale': 28000, 'created_at': '2025-11-22'}
         ]
 
@@ -22,7 +35,7 @@ class Command(BaseCommand):
             add_product = Product(
                 name=product['name'],
                 description=product['description'],
-                category=cat_1,
+                category=product['category'],
                 price_sale=product['price_sale'],
                 created_at=product['created_at']
             )
