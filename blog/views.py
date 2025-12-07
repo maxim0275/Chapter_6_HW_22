ec37301fe4262d62183fff70dfcb23ec7b99584e
maxim0275 <maxim0275@yandex.ru>
2025-12-08 00:00:00 +0500

    c_3

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
+import blog
 from blog.models import Blog
 
 
@@ -16,18 +17,29 @@ class BlogListView(ListView):
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
 
+    def get_queryset(self):
+        return Blog.objects.filter(publication_flag=True)
+
 
 class BlogDetailView(DetailView):
     model = Blog
     template_name = 'blog/blog_detail.html'
     context_object_name = 'blog'
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.number_of_views += 1
+        obj.save()
+        return obj
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ['title', 'content', 'publication_flag']
     template_name = 'blog/blog_form.html'
-    success_url = reverse_lazy('blog:blog_list')
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('blog:blog_detail', args=(self.object.pk,))
 
 
 class BlogDeleteView(DeleteView):
