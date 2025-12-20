ccd8ac14ed6f46e2732f2471a7f681cf295f85b4
maxim0275 <maxim0275@yandex.ru>
2025-12-20 12:58:23 +0500

    c_4

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,9 @@ from catalog.views import ProductListView, ProductDetailView, ContactTemplateVie
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='products_list'),
+    path('list', ProductListView.as_view(), name='products_list'),
+    path('list_f', ProductListView.as_view(), name='products_list_f'),
     path('product_detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('product_detail_f/<int:pk>', ProductDetailView.as_view(), name='product_detail_f'),
     path('contacts_new/', ContactTemplateView.as_view(), name='contacts_new')
 ]
